8487d7b2ce114980dc17321beff0b1d16fadc87e
Muazzam Waheed <waheedmuazzam@gmail.com>
2026-03-23 21:49:03 -0400

    added header files

---
+++ modules/algorithms.h
@@ -0,0 +1,12 @@
+#ifndef ALGORITHMS_H
+#define ALGORITHMS_H
+
+
+void bubbleSort(int arr[], int n);
+void insertionSort(int arr[], int n);
+void radixSort(int arr[], int n);
+void quickSort(int arr[], int n);
+
+
+
+#endif
